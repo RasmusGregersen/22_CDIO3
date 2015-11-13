f2d9425cb404fd5441386fab0d20803bec25f915
Hannibal Moulvad <Hannibal Moulvad>
2015-11-13 12:31:45 +0100

    Forsøgt at lave et If/else statement, i Tax.classen. Der skal tjekke om hvad der bedst kan betale sig for spillerens beholdning

--- src/game/Tax.java
+++ src/game/Tax.java
@@ -2,19 +2,36 @@ package game;
 
 public class Tax extends Field {
 
-	private int taxedAmount;
+	private double taxedAmount;
+	private double percentage = 1.0;
 	private int taxRate;
-	
-	public Tax(String name, int taxedAmount, int taxRate) 
+
+	Player player = new Player();
+
+	public Tax(String name, double taxedAmount, int taxRate) 
 	{
 		super(name);
 		this.taxedAmount = taxedAmount;
 		this.taxRate = taxRate;
 	}
 
+	public int TaxedTotalAssets() {
+
+		if (player.getBalance()*(taxedAmount) < taxRate)
+
+			player.getBalance() = player.getBalance()*(percentage-taxedAmount);
+
+		else if (player.getBalance()*(taxedAmount) > taxRate)
+
+			player.getBalance() = player.getBalance()-taxRate;
+
+		return player.getBalance();
+	}
+
+
 	@Override
 	public void landOnField(Player player) {
-		
+
 	}
-	
+
 }
