fe112f896dbbfa9975926f9adb4b8df7d17bfbcb
Rasmus Gregersen <rasmus.gregersen@live.dk>
2015-11-16 14:49:01 +0100

    Updated game class. And moved diagram out of the game package

--- src/game/Game.java
+++ src/game/Game.java
@@ -17,84 +17,39 @@ import desktop_resources.GUI;
 public class Game {
 
 	public static void main(String[] args) {
-
-		// Creates new entities of our subclasses.
-		DiceCup dicecup = new DiceCup();
-		Player[] player = { new Player(), new Player(), new Player(), new Player(), new Player(), new Player() };
-
-		Language language = new Language();
-
-		int players = GUI.getUserInteger("How many players do you wish to play", 2 , 6);	
 		
-		boolean namecheck = false;
-		while (namecheck == false) {
-			String name1 = GUI.getUserString(language.getPlayer1());
-			String name2 = GUI.getUserString(language.getPlayer2());
-			if (players > 2) {
-				String name3;
-				name3 = GUI.getUserString(language.getPlayer3());
-			}
-			if (players > 3) {
-				String name4;
-				name4 = GUI.getUserString(language.getPlayer4());
-			}
-			if (players > 4) {
-				String name5;
-				name5 = GUI.getUserString(language.getPlayer5());
-			}
-			if (players > 5) {
-				String name6;
-				name6 = GUI.getUserString(language.getPlayer6());
-			}
+		GameBoard gameboard = new GameBoard();
+		DiceCup dicecup = new DiceCup();
+		Language language = new Language();
+		
+		int players = GUI.getUserInteger("How many players do you wish to play", 2 , 6);	
+		Player[] player = new Player[players];
 
-			if (name1.length() < 1 || name1.length() > 15 || name1.indexOf(" ") == 0)
-				name1 = GUI.getUserString(language.getInvalid1());	
-
-			else if (name2.length() < 1 || name2.length() > 15 || name2.indexOf(" ") == 0)
-				name2 = GUI.getUserString(language.getInvalid2());
-			// Player names must not be identical. (Ignores case)
-			else if (name1.equalsIgnoreCase(name2))
-				name2 = GUI.getUserString(language.getnotEqual());
-			else
-				namecheck = true;
+		for (int i=0; i < players; i++) {
+			String name = GUI.getUserString("Please enter the name of player" + i+1);
+			if (name.length() < 1 || name.length() > 15 || name.indexOf(" ") == 0)
+				GUI.showMessage("Invalid name!");
+			else 
+			{
+				GUI.addPlayer(name, player[i].getBalance());
+				player[i].setName(name);
+			}
+			// Mangler at tage højde for at de skal have forskellige navne + GUI car er tilfældige.
 		}
-
-		Car car1 = new Car.Builder()
-				.primaryColor(Color.ORANGE)
-				.secondaryColor(Color.BLACK)
-				.typeRacecar()
-				.patternDotted()
-				.build();
-
-
-		Car car2 = new Car.Builder()
-				.primaryColor(Color.CYAN)
-				.secondaryColor(Color.BLACK)
-				.typeRacecar()
-				.patternDotted()
-				.build();
-
-		String name1 = GUI.getUserString(language.getPlayer1());
-		String name2 = GUI.getUserString(language.getPlayer2());
-
-		player[1].setName(name1);
-		player[2].setName(name2);
-
-		GUI.addPlayer(player1.getName(), player1.getBalance(), car1 );
-		GUI.addPlayer(player2.getName(), player2.getBalance(), car2 );
-
-		Player next = player1;
-
-		// while loop that runs until a winner is found. (win == true).
+		
 		boolean win = false;
-		while (player[1].getBalance() < 3000 && p2.getPoints() < 3000) //WINCONDITION
-
+		while (win == false)
 		{
-			tur(player[0]);
-			tur(player[1]);
-			
+			turn(player[0]);
+			turn(player[1]);
+			turn(player[2]);
+			turn(player[3]);
+			turn(player[4]);
+			turn(player[5]);
 		}
 		}
 
+	private static void turn(Player player) {
+		// Regler
 	}
 }
