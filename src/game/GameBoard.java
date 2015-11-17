42f258b8853029efdf8d758414e645ca14af484c
Rasmus Gregersen <rasmus.gregersen@live.dk>
2015-11-17 14:00:34 +0100

    GUI updated with color after field type

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -12,14 +12,13 @@ public class GameBoard {
 	{
 		desktop_fields.Field[] fieldGUI = new desktop_fields.Field[21];
 		Field[] fieldValue = new Field[21];
-		Color color = Color.RED;
 		fieldValue[0] = new Territory("Tribe Encampment", 100, 1000);
 		fieldGUI[0] = new Street.Builder()
 				.setTitle(fieldValue[0].getName())
 				.setDescription(fieldValue[0].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[0]).getPrice())
 				.setRent(""+((Ownable)fieldValue[0]).getRent())
-				.setBgColor(Color.white)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[1] = new Territory("Crater", 300, 1500);
@@ -28,7 +27,7 @@ public class GameBoard {
 				.setDescription(fieldValue[1].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[1]).getPrice())
 				.setRent(""+((Ownable)fieldValue[1]).getRent())
-				.setBgColor(Color.orange)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[2] = new Territory("Mountain", 500, 2000);
@@ -37,7 +36,7 @@ public class GameBoard {
 				.setDescription(fieldValue[2].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[2]).getPrice())
 				.setRent(""+((Ownable)fieldValue[2]).getRent())
-				.setBgColor(Color.blue)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[3] = new Territory("Cold Desert", 700, 3000);
@@ -46,7 +45,7 @@ public class GameBoard {
 				.setDescription(fieldValue[3].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[3]).getPrice())
 				.setRent(""+((Ownable)fieldValue[3]).getRent())
-				.setBgColor(Color.green)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[4] = new Territory("Black Cave", 1000, 4000);
@@ -55,7 +54,7 @@ public class GameBoard {
 				.setDescription(fieldValue[4].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[4]).getPrice())
 				.setRent(""+((Ownable)fieldValue[4]).getRent())
-				.setBgColor(Color.yellow)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[5] = new Territory("The Werewall", 1300, 4300);
@@ -64,7 +63,7 @@ public class GameBoard {
 				.setDescription(fieldValue[5].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[5]).getPrice())
 				.setRent(""+((Ownable)fieldValue[5]).getRent())
-				.setBgColor(Color.red)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[6] = new Territory("Mountain Village", 1600, 4750);
@@ -73,7 +72,7 @@ public class GameBoard {
 				.setDescription(fieldValue[6].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[6]).getPrice())
 				.setRent(""+((Ownable)fieldValue[6]).getRent())
-				.setBgColor(Color.DARK_GRAY)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[7] = new Territory("South Citadel", 2000, 5000);
@@ -82,7 +81,7 @@ public class GameBoard {
 				.setDescription(fieldValue[7].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[7]).getPrice())
 				.setRent(""+((Ownable)fieldValue[7]).getRent())
-				.setBgColor(Color.LIGHT_GRAY)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[8] = new Territory("Palace Gates", 2600, 5500);
@@ -91,7 +90,7 @@ public class GameBoard {
 				.setDescription(fieldValue[8].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[8]).getPrice())
 				.setRent(""+((Ownable)fieldValue[8]).getRent())
-				.setBgColor(Color.cyan)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[9] = new Territory("Tower", 3200, 6000);
@@ -100,7 +99,7 @@ public class GameBoard {
 				.setDescription(fieldValue[9].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[9]).getPrice())
 				.setRent(""+((Ownable)fieldValue[9]).getRent())
-				.setBgColor(Color.magenta)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[10] = new Territory("Castle", 4000, 8000);
@@ -109,7 +108,7 @@ public class GameBoard {
 				.setDescription(fieldValue[10].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[10]).getPrice())
 				.setRent(""+((Ownable)fieldValue[10]).getRent())
-				.setBgColor(Color.WHITE)
+				.setBgColor(Color.CYAN)
 				.build();
 
 		fieldValue[11] = new Refuge ("Walled City", 5000);
@@ -117,7 +116,7 @@ public class GameBoard {
 				.setTitle(fieldValue[11].getName())
 				.setDescription(fieldValue[11].getClass().getSimpleName())
 				.setSubText(""+((Refuge)fieldValue[11]).getBonus())
-				.setBgColor(Color.WHITE)
+				.setBgColor(Color.GREEN)
 				.build();
 
 		fieldValue[12] = new Refuge ("Monastery", 500);
@@ -125,7 +124,7 @@ public class GameBoard {
 				.setTitle(fieldValue[12].getName())
 				.setDescription(fieldValue[12].getClass().getSimpleName())
 				.setSubText(""+((Refuge)fieldValue[12]).getBonus())
-				.setBgColor(Color.PINK)
+				.setBgColor(Color.GREEN)
 				.build();
 
 		fieldValue[13] = new LaborCamp ("Huts in the mountain", 100, 2500);
@@ -134,7 +133,7 @@ public class GameBoard {
 				.setDescription(fieldValue[13].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[13]).getPrice())
 				.setRent(""+((LaborCamp)fieldValue[13]).getBaseRent())
-				.setBgColor(Color.white)
+				.setBgColor(Color.ORANGE)
 				.build();
 
 		fieldValue[14] = new LaborCamp ("The Pit", 100, 2500);
@@ -143,7 +142,7 @@ public class GameBoard {
 				.setDescription(fieldValue[14].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[14]).getPrice())
 				.setRent(""+((LaborCamp)fieldValue[14]).getBaseRent())
-				.setBgColor(Color.white)
+				.setBgColor(Color.ORANGE)
 				.build();
 
 		fieldValue[15] = new Tax ("Goldmine", 2000, 0);
@@ -151,7 +150,7 @@ public class GameBoard {
 				.setTitle(fieldValue[15].getName())
 				.setDescription(fieldValue[15].getClass().getSimpleName())
 				.setSubText(""+((Tax)fieldValue[15]).getTaxedAmount())
-				.setBgColor(Color.blue)
+				.setBgColor(Color.RED)
 				.build();
 
 		fieldValue[16] = new Tax ("Caravan", 4000, 0.1);
@@ -159,7 +158,7 @@ public class GameBoard {
 				.setTitle(fieldValue[16].getName())
 				.setDescription(fieldValue[16].getClass().getSimpleName())
 				.setSubText(""+((Tax)fieldValue[16]).getTaxedAmount())
-				.setBgColor(Color.blue)
+				.setBgColor(Color.RED)
 				.build();
 
 		fieldValue[17] = new Fleet ("Second Sail", 1, 4000);
@@ -168,7 +167,7 @@ public class GameBoard {
 				.setDescription(fieldValue[17].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[17]).getPrice())
 				.setRent(""+((Ownable)fieldValue[17]).getRent())
-				.setBgColor(Color.blue)
+				.setBgColor(Color.BLUE)
 				.build();
 
 		fieldValue[18] = new Fleet ("Sea Grover", 1, 4000);
@@ -177,7 +176,7 @@ public class GameBoard {
 				.setDescription(fieldValue[18].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[18]).getPrice())
 				.setRent(""+((Ownable)fieldValue[18]).getRent())
-				.setBgColor(Color.blue)
+				.setBgColor(Color.BLUE)
 				.build();
 
 		fieldValue[19] = new Fleet ("The Buccaneers", 1, 4000);
@@ -186,7 +185,7 @@ public class GameBoard {
 				.setDescription(fieldValue[19].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[19]).getPrice())
 				.setRent(""+((Ownable)fieldValue[19]).getRent())
-				.setBgColor(Color.blue)
+				.setBgColor(Color.BLUE)
 				.build();
 
 		fieldValue[20] = new Fleet ("Private armade", 1, 4000);
@@ -195,7 +194,7 @@ public class GameBoard {
 				.setDescription(fieldValue[20].getClass().getSimpleName())
 				.setSubText(""+((Ownable)fieldValue[20]).getPrice())
 				.setRent(""+((Ownable)fieldValue[20]).getRent())
-				.setBgColor(Color.blue)
+				.setBgColor(Color.BLUE)
 				.build();
 
 		GUI.create(fieldGUI);
